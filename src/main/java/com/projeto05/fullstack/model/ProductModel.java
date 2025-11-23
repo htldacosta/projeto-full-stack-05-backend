2026b7780ed6373760c0555106a83352d670fed7
htldacosta <hitaludcosta@gmail.com>
2025-11-23 17:31:53 -0400

    the class ProductModel was created

---
+++ src/main/java/com/projeto05/fullstack/model/ProductModel.java
@@ -0,0 +1,21 @@
+package com.projeto05.fullstack.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class ProductModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String name;
+    private String marca;
+
+
+}
